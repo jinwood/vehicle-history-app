22d3b4248664c7f0420714ad90c6e73f50049291
jinwood <jinw@protonmail.com>
2022-05-18 22:17:53 +0100

    Fiddling around

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import { Amplify } from "aws-amplify"
+import { Authenticator } from "@aws-amplify/ui-react"
+import "@aws-amplify/ui-react/styles.css"
+import awsExports from "../src/aws-exports"
+import type { AppProps } from "next/app"
+import { config } from "process"
+
+Amplify.configure({ ...config, ssr: true })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <Component {...pageProps} />
+        </div>
+      )}
+    </Authenticator>
+  )
 }
 
 export default MyApp
